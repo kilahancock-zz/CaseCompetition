c84ad6f4c3b09059c9760c751967a26a8685f5cd
Charles Schwartz <chschwartz@redventures.com>
2020-06-08 09:23:04 -0500

    adds client server skeleton

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/api/test", (req, res) => {
+    const testObject = [
+        {id: 1, name: "test1"},
+        {id: 2, name: "test2"}
+    ];
+
+    res.json(testObject);
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
