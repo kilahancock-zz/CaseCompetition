b35533fa9621ee89835524c0591b1d24e3c492b6
Andrew Dame <adame@redventures.com>
2020-06-08 14:48:38 -0400

    adds ip address conversion

--- handlers/users.js
+++ handlers/users.js
@@ -1,11 +1,20 @@
 const db = require('../models');
+const iplocate = require("node-iplocate");
 
-async function userHandler(req, res, next){
+function userHandler(req, res, next){
   try {
-    let newUser = await db.User.create({
-      ipAddress: req.connection.remoteAddress
+    iplocate(req.connection.remoteAddress).then(async function(results) {
+      let newUser = await db.User.create({
+          ip: results.ip,
+          country: results.country,
+          country_code: results.country_code,
+          city: results.city,
+          time_zone: results.time_zone,
+          postal_code: results.postal_code,
+          state: results.subdivision
+      });
+      return res.status(200).json(newUser);
     });
-    return res.status(200).json(newUser);
   }
   catch{
 
