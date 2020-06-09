0d02da3a50ae221444f2dd16b3343971dacf5783
alanzino12 <annalanzino@outlook.com>
2020-06-09 16:53:56 -0400

    added modal :100:

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 //import TestComponent from "./components/TestComponent";
 import Home from "./components/Home";
+import StreamPair from './pages/StreamPair';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Home/>
+        {/* <StreamPair/> */}
       </div>
     );
   }
