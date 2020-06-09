402b93bcbcc70d7a18492043b5b2d92d7bee3209
Andrew Dame <adame@redventures.com>
2020-06-09 10:57:35 -0400

    removes component tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 import TestComponent from "./components/TestComponent";
-import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Home/>
+        <TestComponent/>
       </div>
     );
   }
