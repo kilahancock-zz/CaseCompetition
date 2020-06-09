ac5cfbb55d945326905df32b62a3f71aa7804e5b
Andrew Dame <adame@redventures.com>
2020-06-09 10:55:53 -0400

    adds provider data to Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import TestComponent from "./components/TestComponent"
+import TestComponent from "./components/TestComponent";
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <TestComponent/>
-      </div>    
+        <Home/>
+      </div>
     );
   }
 }
