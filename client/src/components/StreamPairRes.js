1be3b6b08cf837098508ce17d43998c0618822ce
alanzino12 <annalanzino@outlook.com>
2020-06-10 10:23:56 -0400

    Added button to redirect to sign up

--- client/src/components/StreamPairRes.js
+++ client/src/components/StreamPairRes.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
-
 
 const StreamPairRes = ({ image, name, link, color }) => {
+    const signUp = () => {
+        window.open(link);
+    }
+
     return (
-        <div style={{ justifyContent: 'center', display: 'flex', flexDirection: 'column' }}>
+        <div style={{textAlign: 'center'}}>
             <img src={image} height={100} style={{ borderRadius: 10 }}/>
-            <div style={{ fontSize: 20 }}>You have been matched with {name}!</div>
+            <div style={{ fontSize: 30, margin: 20 }}>You have been matched with {name}!</div>
             <div>
-                <button style={{ backgroundColor: color, color: 'white', borderRadius: 5, fontSize: 15, width: 200 }}>Sign Up</button>
+                <button onClick={signUp} className='stream-button' style={{ backgroundColor: color, height: 50, color: 'white', borderRadius: 5, fontSize: 20, width: 200, border: 'none', cursor: 'pointer', overflow: 'hidden', outline: 'none', boxShadow: 'none' }}>Sign Up</button>
             </div>
         </div>
     )
