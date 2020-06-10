9fbec3de88017cd071b4b699ca60577b4f442f7a
Andrew Dame <adame@redventures.com>
2020-06-10 09:19:24 -0400

    adds starter for map vis

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Map from './components/Map';
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<Map/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
